eb870863a47ca2e25a9177941bfc3a4c33467b70
Henrique Matheus <mpa.henrique@gmail.com>
2016-12-08 15:12:30 -0200

    Default static file update and server script improvements

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 function init(path, port){
-	path = path || './',
-	port = port || 3000;	
-	var http         = require("http")
-	,	fileSystem   = require("fs");
+    path = path || './',
+    port = port || 3000;    
+    var  http       = require("http")
+    ,    fileSystem = require("fs");
 
-	http.createServer(function(request, response){
+    http.createServer((request, response) => {
 
-		console.log(request.method, request.url);
+        console.log(request.method, request.url);
 
-		if(request.url == '/') request.url += "index.html";
+        if(request.url == '/') request.url += "index.html";
 
-		fileSystem.readFile(path + request.url, function(err, fileData){
-			var status = err ? 404 : 200;
-			response.writeHead(status);
-			if(!err){ // Found file
-				response.end(fileData);
-			} else { // Not found file
-				response.end("Not found!");
-			}
-			console.log(status.toString(), request.url);
-		});
-	}).listen(port);
+        fileSystem.readFile(path + request.url, (err, fileData) => {
+            var status = err ? 520 : 200; // 520 Unknown Error
+            response.writeHead(status);
+
+            console.log(status.toString(), request.url);
+
+            if(!err) return response.end(fileData); // Found file
+            response.end("Not found!"); // Not found file or other error
+            
+        });
+    }).listen(port);
 }
 
-module.exports = init
+module.exports = init;
